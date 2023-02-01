2d98a7e46fbea9ba921b134f84a7182d54b0048a
SayemAbdullah1 <dxsayem0@gmail.com>
2023-02-01 15:54:45 +0600

    open modal for book appointment

--- src/pages/Appointment/AppointmentOptions/ApointmentOption.js
+++ src/pages/Appointment/AppointmentOptions/ApointmentOption.js
@@ -5,11 +5,11 @@ const ApointmentOption = ({ appointmentOption }) => {
     return (
         <div className="card lg:shadow-xl">
             <div className="card-body text-center">
-                <h2 className="text-2xl font-bold text-center">{name}</h2>
+                <h2 className="text-2xl text-secondary font-bold text-center">{name}</h2>
                 <p>{slots.length > 0 ? slots[0] : 'try another day'}</p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'}</p>
                 <div className="card-actions justify-center">
-                    <button className="btn btn-primary">Book Appointment</button>
+                    <label htmlFor="book-modal" className="btn btn-primary">Book Appointment</label>
                 </div>
             </div>
         </div>
--- src/pages/Appointment/AppointmentOptions/AppointmentOptions.js
+++ src/pages/Appointment/AppointmentOptions/AppointmentOptions.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ApointmentOption from './ApointmentOption';
+import BookingModal from './BookingModal/BookingModal';
 
 const AppointmentOptions = () => {
     const [appointmentOptions, setAppointmentOptions] = useState([])
@@ -9,13 +10,16 @@ const AppointmentOptions = () => {
         .then(data => setAppointmentOptions(data))
     }, [])
     return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 '>
-            {
-                appointmentOptions.map(appointmentOption => <ApointmentOption
-                    key={appointmentOption.id}
-                    appointmentOption={appointmentOption}
-                ></ApointmentOption>)
-            }
+        <div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 '>
+                {
+                    appointmentOptions.map(appointmentOption => <ApointmentOption
+                        key={appointmentOption.id}
+                        appointmentOption={appointmentOption}
+                    ></ApointmentOption>)
+                }
+            </div>
+            <BookingModal></BookingModal>
         </div>
     );
 };
