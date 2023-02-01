03960e8162f9c1073300aab0d63dd3302963ccd1
SayemAbdullah1 <dxsayem0@gmail.com>
2023-02-02 00:27:32 +0600

    complete modal

--- src/pages/Appointment/AppointmentOptions/ApointmentOption.js
+++ src/pages/Appointment/AppointmentOptions/ApointmentOption.js
@@ -6,7 +6,7 @@ const ApointmentOption = ({ appointmentOption, setTreatment }) => {
         <div className="card lg:shadow-xl">
             <div className="card-body text-center">
                 <h2 className="text-2xl text-secondary font-bold text-center">{name}</h2>
-                <p>{slots.length > 0 ? slots[0] : 'try another day for treatment'}</p>
+                <p>{slots.length > 0 ? slots[0] : 'try another day'}</p>
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'}</p>
                 <div className="card-actions justify-center">
                     <label  
--- src/pages/Appointment/AppointmentOptions/BookingModal/BookingModal.js
+++ src/pages/Appointment/AppointmentOptions/BookingModal/BookingModal.js
@@ -3,27 +3,49 @@ import { format } from 'date-fns';
 
 const BookingModal = ({ treatment, selected }) => {
     const date = format(selected, 'PP')
-    const {name, slots} = treatment
+    const {name: treatmentName, slots} = treatment
+
+    const handleBooking = event =>{
+        event.preventDefault()
+        const form = event.target;
+        const slot = form.slot.value;
+        const name = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+
+        
+        
+        const booking = {
+        treatment: treatmentName,
+        patient: name,
+        appointmentDate: date,
+        slot,
+        email,
+        phone
+    }
+    console.log(booking);
+    }
     return (
         <>
             <input type="checkbox" id="book-modal" className="modal-toggle bg-" />
             <div className="modal">
                 <div className="modal-box relative">
                     <label htmlFor="book-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold">{name}</h3>
-                    <form className='grid grid-cols-1 gap-3 mt-4'>
-                        <input type="text" value={date} className="input w-full input-bordered bg-slate-300" disabled/>
-                        <select className="select select-bordered w-full">
+                    <h3 className="text-lg font-bold">{treatmentName}</h3>
+
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-4'>
+                        <input type='text' value={date} className="input w-full input-bordered bg-slate-300" disabled/>
+                        <select name='slot' className="select select-bordered w-full">
                             {
                                 slots.map(slot => <option value={slot}>{slot}</option>)
                             }
                            
                         </select>
-                        <input type="text" placeholder="Full Name" className="input w-full input-bordered" />
-                        <input type="text" placeholder="Phone" className="input w-full input-bordered" />
-                        <input type="text" placeholder="Email" className="input w-full input-bordered" />
+                        <input name='name' type="text" placeholder="Full Name" className="input w-full input-bordered" />
+                        <input name='phone' type="text" placeholder="Phone" className="input w-full input-bordered" />
+                        <input name='email' type="text" placeholder="Email" className="input w-full input-bordered" />
                         <br />
-                        <input type="button" className=' btn btn-neutral w-full' value="Submit" />
+                        <input type="submit"  className=' btn btn-neutral w-full' value="Submit" />
                     </form>
                 </div>
             </div>
