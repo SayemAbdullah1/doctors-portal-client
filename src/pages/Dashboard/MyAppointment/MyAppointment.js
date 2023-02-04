32f0aaea6c9dbb255e47c5a5546f84c88032ed23
SayemAbdullah1 <dxsayem0@gmail.com>
2023-02-04 21:34:37 +0600

    show initial myappointment table

--- src/pages/Dashboard/MyAppointment/MyAppointment.js
+++ src/pages/Dashboard/MyAppointment/MyAppointment.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React, { useContext } from 'react';
+import { authContext } from '../../../context/AuthProvider/AuthProvider';
 
 const MyAppointment = () => {
+    const {user} = useContext(authContext)
+    const url = `http://localhost:5000/bookings?email=${user?.email}`
+
+    const {data: bookings = []} = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async ()=>{
+           const res = await fetch(url)
+           const data = await res.json()
+           return data;
+        }
+    })
+
     return (
         <div>
             <h2 className='text-3xl'>My appointment</h2>
-            
+
             <div className="overflow-x-auto">
                 <table className="table w-full">
                     
@@ -12,32 +26,26 @@ const MyAppointment = () => {
                         <tr>
                             <th></th>
                             <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Treatment</th>
+                            <th>Date</th>
+                            <th>Time</th>
                         </tr>
                     </thead>
                     <tbody>
                         
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Blue</td>
-                        </tr>
+                        {
+                            bookings.map((booking, i) =>
+                                <tr className="hover">
+                                    <th>{i+1}</th>
+                                    <td>Hart Hagerty</td>
+                                    <td>Desktop Support Technician</td>
+                                    <td>Purple</td>
+                                    <td>Purple</td>
+                                </tr>
+                            )
+                        }
                         
-                        <tr className="hover">
-                            <th>2</th>
-                            <td>Hart Hagerty</td>
-                            <td>Desktop Support Technician</td>
-                            <td>Purple</td>
-                        </tr>
                         
-                        <tr>
-                            <th>3</th>
-                            <td>Brice Swyre</td>
-                            <td>Tax Accountant</td>
-                            <td>Red</td>
-                        </tr>
                     </tbody>
                 </table>
             </div>
