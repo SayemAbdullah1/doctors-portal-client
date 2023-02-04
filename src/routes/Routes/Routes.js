8074cbc77bb69809ad9f42395e3603699668299e
SayemAbdullah1 <dxsayem0@gmail.com>
2023-02-04 20:47:05 +0600

    add my appointment in dashboard

--- src/routes/Routes/Routes.js
+++ src/routes/Routes/Routes.js
@@ -2,6 +2,7 @@ import DashboardLayout from "../../layout/DashboardLayout";
 import Main from "../../layout/Main";
 import Appointment from "../../pages/Appointment/Appointment/Appointment";
 import Dashboard from "../../pages/Dashboard/Dashboard/Dashboard";
+import MyAppointment from "../../pages/Dashboard/MyAppointment/MyAppointment";
 import Home from "../../pages/Home/Home/Home";
 import Login from "../../pages/Login/Login";
 import SignUp from "../../pages/SignUp/SignUp";
@@ -38,7 +39,7 @@ const { createBrowserRouter } = require("react-router-dom");
         children: [
             {
                 path: '/dashboard',
-                element: <Dashboard></Dashboard>
+                element: <MyAppointment></MyAppointment>
             }
         ]
     }
