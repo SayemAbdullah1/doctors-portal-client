a3306e1d4d1a130cc745c4b6a7385fbe2d034a06
SayemAbdullah1 <dxsayem0@gmail.com>
2023-02-05 19:00:15 +0600

    save user access token in localhost

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -47,10 +47,20 @@ const SignUp = () => {
         })
         .then(res => res.json())
         .then(data => {
-            console.log('save user', data)
-            navigate('/')
+            // console.log('save user', data)
+            getUserToken(email)
         })
-    
+    }
+
+    const getUserToken = email => {
+        fetch(`http://localhost:5000/jwt?email=${email}`)
+            .then(res => res.json())
+            .then(data => {
+                if (data.accessToken) {
+                    localStorage.setItem('accessToken', data.accessToken)
+                    navigate('/')
+                }
+            })
     }
     return (
         <div className='h-[800px] flex justify-center items-center'>
